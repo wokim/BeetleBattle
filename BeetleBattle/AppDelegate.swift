816c4d06fddd8a224f3e0b8d8998be28f7d26246
Kim, Wonshik (Alex) <wonshikkim.kr@gmail.com>
2021-07-17 10:16:51 +0900

    Initial Commit

---
+++ BeetleBattle/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BeetleBattle
+//
+//  Created by Wonshik KIM on 2021/07/17.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
